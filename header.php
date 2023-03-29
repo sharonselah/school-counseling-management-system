0742dc6f384049a8f8ac6851ec696042f86e89d1
Sharon Selah <sellaahsharon2001@gmail.com>
2023-03-29 18:10:12 +0300

    Created the Home Page

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Header</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            CUEA Counseling
+        </div>
+
+        <ul>
+            <li><a href="">Providers</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Resources</a></li>
+            <li><a href="">Selfcare</a></li>
+            <li><a href="">Conditions</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+
+        <div class="button">
+            <a href="">Book an Appointment</a>
+        </div>
+    </header>
---
+++ home.php
@@ -0,0 +1,235 @@
+<?php include 'header.php' ?>
+
+
+    <div class="call-to-action">
+
+   
+
+            <div class="emotions">
+                    <p>Depressed? 	&#128531</p>
+                    <p>Scared? &#128552</p>
+                    <p>Confused? &#128528</p>
+            </div>
+       
+         
+            <h1 style=" font-size: 68px; margin-bottom: 0px; margin-top: 0px; ">Just Breath & Relax</h1>
+            <p style=" color: #848884; line-height: 36px; text-align: center; ">The CUEA Counseling team provides high quality mental health services using an<br>
+            evidence-based approach and Christian values. <br> We believe in early identification</p>
+
+         <div class="button">
+            <a href="">Book an Appointment</a>
+        </div>
+    </div>
+
+
+   
+
+
+
+    <div class="services-container">
+        <h1>Our Services</h1>
+        <p style="color: #848884;"> CUEA's expansive network of licensed clinicians offers a wide variety of services </p>
+
+        <div class="services">
+            <div class="service">
+                <div class="image">
+                    <p>&#10004</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Transition to College</h2>
+                    <p>Leaving home for college can be stressful for first-year college students.
+                         Feelings of isolation and homesickness are common .
+                    </p>
+                </div>
+
+                
+            </div>
+
+            <div class="service">
+                <div class="image">
+                    <p>&#10006</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Alcohol Use</h2>
+                    <p> University alcoholism affects millions fo students each year. Frequent heavy
+                        drinking causes great emotional and physical damages. 
+                    </p>
+                </div>
+
+                
+            </div>
+
+            <div class="service">
+                <div class="image">
+                    <p>&#10013</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Anger Management</h2>
+                    <p>Anger should not be suppressed and treatment focuses on problem solving and acceptance-based strategies
+                        and operates within a framework.
+                    </p>
+                </div>
+
+                
+            </div>
+
+            <div class="service">
+                <div class="image">
+                    <p>&#10036</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Depression Management</h2>
+                    <p>
+                    The stress that comes with academia — including financial worries, pressure to get a good job after school, 
+                    and failed relationships — is enough to force some students to leave college or worse.
+                
+                    </p>
+                </div>
+
+                
+            </div>
+
+            <div class="service">
+                <div class="image">
+                    <p>&#10052</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Anxiety Management</h2>
+                    <p>
+                    The combination of academic pressure, moving away from home, new social situations, 
+                    and financial stressors can create the perfect storm for anxiety.
+                    </p>
+                </div>
+
+                
+            </div>
+            <div class="service">
+                <div class="image">
+                    <p>&#10068</p> 
+                </div>
+
+                <div class="content">
+
+                    <h2>Relationship Conflict</h2>
+                    <p>
+                    Social rejection in one's teens may shape their ability to trust.
+                     Some teens are bullied or treated as outcasts by peers. This can influence later relationships.
+                    </p>
+                </div>
+
+                
+            </div>
+
+            
+            <div class="button">
+                <a href=""> View All Services</a>
+            </div>
+
+            
+        </div>
+      
+</div>
+
+
+<!-- Counseling Team -->
+
+
+<div class="counselors-container">
+
+<h1>Our Counselors</h1>
+
+<p style="color: #848884;">Our Work is to Help Students Dealing with a Variety of Mental Health Conditions</p>
+   
+
+
+<div class="counselors">
+    <div class="counselor">
+        <img src="Images/florence-lec.jpg" alt="image">
+        <h2>Florence Wamahiga (PhD)</h2>
+        <p>Florence is a senior lecturer in the department of counseling psychology.
+        She is well versed with issues concerning school dropout and HIV counseling
+       </p>
+
+        <button>Book Appointment</button>
+
+    </div>
+    <div class="counselor">
+        <img src="Images/asatsa-lec.jpg" alt="">
+        <h2>Stephen Asatsa <br>(PhD)</h2>
+        <p>
+        Stephen is the Head of the Department of Counselling Psychology at CUEA. 
+        He is an expert at dealing with behavioral psychology; depression and anxiety. 
+        </p>
+
+        <button>Book Appointment</button>
+
+    </div>
+    <div class="counselor">
+        <img src="Images/elijah-lec.jpg" alt="">
+        <h2>Elijah Macharia <br> (PhD)</h2>
+        <p>
+        Elijah Macharia is a Lecturer at CUEA. He is well-experienced in dealing
+         with poverty trauma, financial instability, food insecurity and the associated effects. 
+       </p>
+
+        <button>Book Appointment</button>
+
+    </div>
+</div>
+
+<div class="button">
+    <a href=""> View All Counselors</a>
+</div>
+</div> <!-- end of counselors --> 
+
+<div class="self-care">
+    <div class="image">
+        <img src="Images/student2.jpg" alt="">
+    </div>
+
+    <div class="content">
+
+    <h2>How and Why to Practice Self-care <br> As A College Student</h2>
+
+    <p>Self-care means taking the time to do things that help you live well 
+        and improve both your physical health and mental health.
+        When it comes to your mental health, self-care can help you manage stress, 
+        lower your risk of illness, and increase your energy.</p>
+    <div class="list">
+        <ul>
+            <li>Healthy Food</li>
+            <li>Daily Walk</li>
+            <li>Drinking Water</li>
+            <li>Daily Exercises</li>
+        </ul>
+
+        <ul style="margin-left: 40px; ">
+            <li>Time Management </li>
+            <li>Quality Time with Family</li>
+            <li>8 Hours of Sleep</li>
+            <li>Nurture Friendships</li>
+        </ul>
+    </div>
+
+    <div class="button">
+            <a href="">Contact Us</a>
+    </div>
+    </div>
+</div>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
